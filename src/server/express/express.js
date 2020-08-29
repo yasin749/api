3873de226f6c90ece446bd80aa16d9e06ecc475e
Yasin UYSAL <yasin.uysal@hepsiburada.com>
2020-08-30 01:47:30 +0300

    Routes path bugfix

--- src/server/express/express.js
+++ src/server/express/express.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var routes = require('./src/routes');
+var routes = require('../../routes');
 
 var app = express();
 
