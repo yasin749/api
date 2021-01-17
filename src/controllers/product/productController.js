0370b18bfbda6f76566140fbbc6879d713ab93ef
Yasin UYSAL <yasin.uysal@hepsiburada.com>
2021-01-17 19:11:55 +0300

    Evaluation seed change, TestFunc deleted,

--- src/controllers/product/productController.js
+++ src/controllers/product/productController.js
@@ -24,11 +24,15 @@ module.exports = {
                         attributes: {
                             include: [
                                 [
-                                    database.literal(`COUNT(case "evaluationGroup->evaluationAttributes->evaluations"."evaluation" when 1 then 1 else null end)`),
+                                    database.literal(
+                                        `COUNT(case "evaluationGroup->evaluationAttributes->evaluations"."evaluation" when 1 then 1 else null end)`
+                                    ),
                                     'upVote'
                                 ],
                                 [
-                                    database.literal(`COUNT(case "evaluationGroup->evaluationAttributes->evaluations"."evaluation" when 0 then 1 else null end)`),
+                                    database.literal(
+                                        `COUNT(case "evaluationGroup->evaluationAttributes->evaluations"."evaluation" when 0 then 1 else null end)`
+                                    ),
                                     'downVote'
                                 ],
                             ]
@@ -46,7 +50,12 @@ module.exports = {
                     }
                 },
             ],
-            group: ['Product.id', 'category.id', 'evaluationGroup.id', 'evaluationGroup->evaluationAttributes.id']
+            group: [
+                'Product.id',
+                'category.id',
+                'evaluationGroup.id',
+                'evaluationGroup->evaluationAttributes.id'
+            ],
         });
 
         if (product) {
@@ -55,54 +64,4 @@ module.exports = {
             response.error(res);
         }
     },
-
-    // @todo delete this
-    testFunc: async function (req, res, next) {
-        const groupId = 1;
-        const productId = 1;
-
-        const product = await database.models.EvaluationGroup.findOne({
-            where: {id: groupId},
-            include: {
-                model: database.models.EvaluationAttribute,
-                as: 'evaluationAttributes',
-                attributes: {
-                    include: [
-                        /*[
-                            database.fn(
-                                "COUNT",
-                                database.col("evaluationAttributes.evaluations.evaluation")
-                            ),
-                            "evu"
-                        ],*/
-                        [
-                            database.literal(`COUNT(case "evaluationAttributes->evaluations"."evaluation" when 1 then 1 else null end)`),
-                            'upVote'
-                        ],
-                        [
-                            database.literal(`COUNT(case "evaluationAttributes->evaluations"."evaluation" when 0 then 1 else null end)`),
-                            'downVote'
-                        ],
-                    ]
-                },
-                through: {
-                    attributes: []
-                },
-                include: {
-                    model: database.models.Evaluation,
-                    as: 'evaluations',
-                    where: {productId: productId},
-                    attributes: [],
-                    required: false,
-                },
-            },
-            group: ['EvaluationGroup.id', 'evaluationAttributes.id']
-        });
-
-        if (product) {
-            response.ok(res, product);
-        } else {
-            response.error(res);
-        }
-    }
 };
--- src/database/seeders/20210109132339-Evaluation.js
+++ src/database/seeders/20210109132339-Evaluation.js
@@ -3,6 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Evaluations', [
+      // USER 1 PRODUCT 1
       {
         userId: 1,
         productId: 1,
@@ -19,23 +20,17 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      // USER 1 PRODUCT 2
       {
         userId: 1,
-        productId: 1,
-        evaluationAttributeId: 3,
-        evaluation: 1,
-        createdAt: new Date(),
-        updatedAt: new Date()
-      },
-      {
-        userId: 1,
-        productId: 1,
-        evaluationAttributeId: 4,
+        productId: 2,
+        evaluationAttributeId: 1,
         evaluation: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
 
+      // USER 2 PRODUCT 1
       {
         userId: 2,
         productId: 1,
@@ -52,19 +47,39 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      // USER 2 PRODUCT 2
       {
         userId: 2,
-        productId: 1,
-        evaluationAttributeId: 3,
+        productId: 2,
+        evaluationAttributeId: 1,
         evaluation: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
+
+      // USER 3 PRODUCT 1
       {
-        userId: 2,
+        userId: 3,
         productId: 1,
-        evaluationAttributeId: 4,
-        evaluation: 1,
+        evaluationAttributeId: 1,
+        evaluation: 0,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        userId: 3,
+        productId: 1,
+        evaluationAttributeId: 2,
+        evaluation: 0,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      // USER 3 PRODUCT 2
+      {
+        userId: 3,
+        productId: 2,
+        evaluationAttributeId: 1,
+        evaluation: 0,
         createdAt: new Date(),
         updatedAt: new Date()
       },
